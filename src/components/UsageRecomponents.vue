41ce71d66b8a905f0d40f70c9d0612cd87aeb887
Junyong <zengjunyong@gmail.com>
2019-09-02 16:05:16 +0800

    how to use recomponents

---
+++ src/components/UsageRecomponents.vue
@@ -0,0 +1,17 @@
+<template>
+    <main>
+        <r-button>hey! btn</r-button>
+    </main>
+</template>
+
+<script>
+    import Vue from 'vue'
+    import 'rebilly-recomponents/dist/recomponents.css'
+    import Recomponents from 'rebilly-recomponents'
+
+    Vue.use(Recomponents)
+
+    export default {
+        name: 'UsageRecomponents',
+    }
+</script>
